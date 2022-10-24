5f527ff5845d7f9aeeabcbfadc2f45cfeaeee105
T-Ito <tito40358@gmail.com>
2022-10-24 13:58:25 +0900

    メール設定

--- app/Http/Controllers/AdoptionController.php
+++ app/Http/Controllers/AdoptionController.php
@@ -10,12 +10,15 @@ class AdoptionController extends Controller
 {
     public function send(Request $request)
     {
-        $introduce = $request->name."様よりホームページ採用のページから面談の申し込みがありました\nemail:".$request->email."\n電話番号:".$request->tel;
+        $introduce = $request->name." 様よりホームページ採用のページから面談の申し込みがありました\nemail:".$request->email."\n電話番号:".$request->tel;
+        $introduce_tosender = $request->name." 様\n下記にてメールを送信しましたので返信をお待ち下さい\nemail:".$request->email."\n電話番号:".$request->tel;
         $message = $request->message;
         $my_url = config('my-url.url');
         $to_email = "tnitoh@global-software.co.jp";
         if ($my_url=="http://global-asagaya.tk") {
             Mail::to($to_email)->send(new Admin($introduce, $message, $my_url));
+            //送信者にも控えを送付する
+            Mail::to($request->email)->send(new Admin($introduce_tosender, $message, $my_url));
         }
         return redirect("articles/5?dis=true")->with('success', '送信完了しました。返信まで少々お待ち下さい。');
     }
--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -11,11 +11,14 @@ class ContactController extends Controller
     public function send(Request $request)
     {
         $introduce = $request->name."様よりホームページお問合せページからメッセージがありました\nemail:".$request->email."\n電話番号:".$request->tel;
+        $introduce_tosender = $request->name." 様\n下記にてメールを送信しましたので返信をお待ち下さい\nemail:".$request->email."\n電話番号:".$request->tel;
         $message = $request->message;
         $my_url = config('my-url.url');
         $to_email = "tnitoh@global-software.co.jp";
         if ($my_url=="http://global-asagaya.tk") {
             Mail::to($to_email)->send(new Admin($introduce, $message, $my_url));
+            //送信者にも控えを送付する
+            Mail::to($request->email)->send(new Admin($introduce_tosender, $message, $my_url));
         }
         return redirect("articles/4?dis=true")->with('success', '送信完了しました。返信まで少々お待ち下さい。');
     }

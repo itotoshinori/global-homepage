3c8d701c859c2f44ea6deddeb71d82ce2bcdc71d
T-Ito <tito40358@gmail.com>
2022-11-04 18:21:56 +0900

    sendgridにメーラー変更

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -160,9 +160,11 @@ class ArticleController extends Controller
         $article->update($update);
         $message = "記事の更新がありました。ご確認ください\n".$this->articles_url.$article->id;
         if ($this->my_url != "http://localhost") {
-            foreach ($this->users as $user) {
-                Mail::to($user->email)->send(new Admin($this->name, $message, $this->my_url));
-            }
+            //foreach ($this->users as $user) {
+            //Mail::to($user->email)->send(new Admin($this->name, $message, $this->my_url));
+            //}
+            //メールテスト用に残す。テスト時コメントアウト
+            Mail::to($this->to_email)->send(new Admin($this->name, $message, $this->my_url));
         }
         return redirect("articles/".$article->id)->with('success', '更新完了しました');
     }
--- app/Mail/Admin.php
+++ app/Mail/Admin.php
@@ -28,7 +28,7 @@ class Admin extends Mailable
      */
     public function build()
     {
-        return $this->from('info@titonet384.sakura.ne.jp')
+        return $this->from('info@global-software.jp')
                     ->subject("グローバルホームページからのお知らせ")
                     ->view('mails.admin')
                     ->with([

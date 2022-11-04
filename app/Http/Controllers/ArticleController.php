86f7342113ab9e2b82d81c164e08cd5926371f4c
T-Ito <tito40358@gmail.com>
2022-11-04 18:45:34 +0900

    sメールコマンド修正

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -78,15 +78,13 @@ class ArticleController extends Controller
             $create['image_detail'] = $image_detail;
         }
         $result = Article::create($create);
-        if ($result) {
+        if ($result && $this->my_url != "http://localhost") {
             $message = "記事の新規登録がありました。ご確認ください。\n".$this->articles_url;
-            if ($this->my_url != "http://localhost") {
-                foreach ($this->users as $user) {
-                    Mail::to($user->to_email)->send(new Admin($this->name, $message, $this->my_url));
-                }
-            }
+            //foreach ($this->users as $user) {
+            //Mail::to($user->to_email)->send(new Admin($this->name, $message, $this->my_url));
+            //}
             //メールテスト用に残す。テスト時コメントアウト
-            //Mail::to($this->to_email)->send(new Admin($this->name, $message, $this->my_url));
+            Mail::to($this->to_email)->send(new Admin($this->name, $message, $this->my_url));
         }
         return redirect()->route('articles.index')->with('success', '新規登録完了しました');
     }
@@ -160,11 +158,11 @@ class ArticleController extends Controller
         $article->update($update);
         $message = "記事の更新がありました。ご確認ください\n".$this->articles_url.$article->id;
         if ($this->my_url != "http://localhost") {
-            //foreach ($this->users as $user) {
-            //Mail::to($user->email)->send(new Admin($this->name, $message, $this->my_url));
-            //}
+            foreach ($this->users as $user) {
+                Mail::to($user->email)->send(new Admin($this->name, $message, $this->my_url));
+            }
             //メールテスト用に残す。テスト時コメントアウト
-            Mail::to($this->to_email)->send(new Admin($this->name, $message, $this->my_url));
+            //Mail::to($this->to_email)->send(new Admin($this->name, $message, $this->my_url));
         }
         return redirect("articles/".$article->id)->with('success', '更新完了しました');
     }

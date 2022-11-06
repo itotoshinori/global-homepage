bb397ce51328effc168a749f240cb28a7b40b3d0
T-Ito <tito40358@gmail.com>
2022-11-06 16:04:57 +0900

    url変更

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -80,12 +80,12 @@ class ArticleController extends Controller
         }
         $result = Article::create($create);
         if ($result && $this->my_url != "http://localhost") {
-            $message = "記事の新規登録がありました。ご確認ください。\n".$this->articles_url;
+            $message = "記事の新規登録がありました。ご確認ください。\n".$this->my_url;
             foreach ($this->users as $user) {
-                Mail::to($user->to_email)->send(new Admin($this->name, $message, $this->my_url));
+                //Mail::to($user->to_email)->send(new Admin($this->name, $message, $this->my_url));
             }
             //メールテスト用に残す。テスト時コメントアウト
-            //Mail::to($this->to_email)->send(new Admin($this->name, $message, $this->my_url));
+            Mail::to($this->to_email)->send(new Admin($this->name, $message, $this->my_url));
         }
         return redirect()->route('articles.index')->with('success', '新規登録完了しました');
     }

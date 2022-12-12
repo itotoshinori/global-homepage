f6744f5547e6b0a314a85cb2a94688a5b06518d2
T-Ito <tito40358@gmail.com>
2022-12-12 13:14:18 +0900

    記事フォント修正

--- resources/views/infos/show.blade.php
+++ resources/views/infos/show.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8" />
 	<title>株式会社グローバル</title>
 	<link rel="stylesheet" href="{{ asset('/css/info.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('/css/info.css') }}">
 </header>
 
 <body>

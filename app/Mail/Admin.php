be8d62f1b522ea4ae490a78c40b46d8dcd54f38b
T-Ito <tito40358@gmail.com>
2022-12-16 15:34:55 +0900

    送信メールアドレス

--- app/Mail/Admin.php
+++ app/Mail/Admin.php
@@ -28,7 +28,7 @@ class Admin extends Mailable
      */
     public function build()
     {
-        return $this->from('tnitoh@global-software.co.jp')
+        return $this->from('info@global-software.co.jp')
                     ->subject("グローバルホームページからのお知らせ")
                     ->view('mails.admin')
                     ->with([

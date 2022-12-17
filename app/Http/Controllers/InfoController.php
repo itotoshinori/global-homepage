e1a9c72b7b1d10cd95b730e99607d66348839c5a
T-Ito <tito40358@gmail.com>
2022-12-18 01:11:04 +0900

    ログイン不具合

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -105,9 +105,10 @@ class InfoController extends Controller
         if ($request->content_dis=="on") {
             $message = "{$info->title}\n$info->body";
             //後で消す
-            $message = $message."\nログイン情報";
+            $message = $message."\n\nログイン情報";
             $message = $message."\nEmail:globalEmail";
             $message = $message."\nパスワード:{$curret_user->note}";
+            $message = $message."\n社内ホームページ\nhttps://global-software.jp/internal/infos";
             //
         } else {
             $message = "「{$info->title}」\nの新規お知らせ情報の登録が社内ホームページにありました。\n下記URLをクリックしてご確認ください。";
@@ -190,9 +191,10 @@ class InfoController extends Controller
         if ($request->content_dis=="on") {
             $message = "{$info->title}\n{$info->body}";
             //後で消す
-            $message = $message."\nログイン情報";
+            $message = $message."\n\nログイン情報";
             $message = $message."\nEmail:globalEmail";
             $message = $message."\nパスワード:{$curret_user->note}";
+            $message = $message."\n社内ホームページ\nhttps://global-software.jp/internal/infos";
             //
         } else {
             $message = "「{$info->title}」\nのお知らせ情報の更新がありました。\n下記URLをクリックしてご確認ください。";
